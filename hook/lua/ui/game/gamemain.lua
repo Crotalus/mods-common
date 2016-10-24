fd3857e1be51f75f0c9198637cbd538daaa8bf43
Crotalus <Crotalus@users.noreply.github.com>
2016-10-25 00:46:32 +0200

    Using select from faf

--- hook/lua/ui/game/gamemain.lua
+++
@@ -1,8 +0,0 @@
-local Select = import('/mods/common/select.lua')
-
-local originalOnSelectionChanged = OnSelectionChanged
-function OnSelectionChanged(oldSelection, newSelection, added, removed)
-    if not Select.IsHidden() then
-        originalOnSelectionChanged(oldSelection, newSelection, added, removed)
-    end
-end
